e1b9b6f09f22368a728f42155565291a34a5710d
jordsek <73316853+jordsek@users.noreply.github.com>
2025-03-24 19:40:45 +0000

    First commit

---
+++ Catgif/CatgifApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatgifApp.swift
+//  Catgif
+//
+//  Created by Jor . on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatgifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
